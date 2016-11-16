3123cb90d8c54f073705b2740cebd0856dd57d57
Ajay Panyala <ajay.panyala@gmail.com>
2016-11-15 23:16:56 -0800

    working version of mercat

--- README.md
+++ README.md
@@ -1,6 +1,19 @@
-Mercat
-------
+Mercat: Python code for k-mer counting.
+=======================================
+  
+>Usage: python mercat.py path-to-input-file kmer-value   
+Example: To compute all 2-mers -> python mercat.py test.faa 2  
 
-python mercat.py path-to-input-file kmer-value  
-> ex: python mercat.py test.fa 2
+- Results are stored in input-file-name.csv and input-file-name_summary.csv  
+   (test.csv and test_summary.csv in the above example)  
+- test.csv contains kmer count for kmers in individual sequences  
+- test_summary.csv contains kmer count for all unique kmers across all sequences in the sample test.fa
  
+ 
+Dependencies
+------------
+Mercat needs pandas library: pip install pandas
+
+TODO
+----
+- Mercat currently takes only 1 file at a time and supports only `faa` and `fasta` formats. FastQ format is not yet supported.
--- mercat.py
+++ mercat.py
@@ -1,52 +1,102 @@
+#!/usr/bin/env python
+
+"""mercat.py: Python code for k-mer counting."""
+"""Usage: python mercat.py path-to-input-file kmer-value """
+"""Example: To compute all 2-mers -> python mercat.py test.faa 2"""
+"""Results are stored in input-file-name.csv and input-file-name_summary.csv
+   (test.csv and test_summary.csv in the above example)"""
+"""test.csv contains kmer count for kmers in individual sequences
+   test_summary.csv contains kmer count for all unique kmers across all sequences in the sample test.fa"""
+
+__author__      = "Ajay Panyala, Richard A. White III"
+__copyright__   = "Copyright 2016"
+
 import sys
 import re
 import os
 import glob
-import csv
+import pandas as pd
+from collections import OrderedDict
 
 inputfile = sys.argv[1]
 kmer = int(sys.argv[2])
 
+bif = os.path.splitext(os.path.basename(inputfile))[0]
+#print bif
 
 def get_all_substrings(input_string):
     length = len(input_string)
-    return [input_string[i:i + kmer] for i in xrange(length-kmer)]
+    return [input_string[i:i + kmer] for i in xrange(length-kmer+1)]
 
 # ipfiles = glob.glob("*.faa")
 # ipfiles.extend(glob.glob("*.fa"))
 
-
-sequences = []
+sequences = OrderedDict()
+#seq_kmers = dict()
 
 with open(inputfile,'r') as f:
     seq = ""
+    sname = ""
     for line in f:
         line = line.strip()
         if line.startswith(">"):
-            if len(seq)>0:
-                sequences.append(seq)
+            if sname: sequences[sname] = ""
+            if seq:
+                sequences[sname] = seq
                 seq = ""
+            sname = line[1:]
         else:
             seq += line
 
-print len(sequences)
+assert sname and seq
+sequences[sname] = seq
+
+print "Number of sequences in " + inputfile + " = "+ str(len(sequences))
 kmerlist = dict()
+kmerlist_all_seq = dict()
+
+
 
 for seq in sequences:
-    sslist = get_all_substrings(seq)
+    cseq = sequences[seq] #Get current sequence
+    sslist = get_all_substrings(cseq) # get all substrings of current sequence
+    kmerlist_all_seq[seq] = dict() #kmer count for each substring of current sequence
+    #print sslist
     for ss in sslist:
-        if ss not in kmerlist: kmerlist[ss] = 0
-        kmerlist[ss] += len(re.findall(r'(?=(%s))' % re.escape(ss), seq))
-
+        if ss not in kmerlist: kmerlist[ss] = 0 #global kmer count for substring ss
+        if ss not in kmerlist_all_seq[seq]: kmerlist_all_seq[seq][ss] = 0 #kmer count for substring in current sequence
+        count = len(re.findall(r'(?=(%s))' % re.escape(ss), cseq))
+        kmerlist[ss] += count #global kmer count for substring ss
+        kmerlist_all_seq[seq][ss] = count #kmer count for substring in current sequence
 
 #print kmerlist
 
-# bif = os.path.basename(inputfile).split[0]
-# print bif
+dfcol = kmerlist.keys()
+dfcol.extend(["length","GC","AT"])
 
+df = pd.DataFrame(0,index=sequences.keys(),columns=dfcol)
 
-with open('output.csv', 'wb') as csv_file:
-    writer = csv.writer(csv_file)
-    for key, value in kmerlist.items():
-       writer.writerow([key, value])
+for seq in sequences:
+    cseq = sequences[seq]
+    len_cseq = float(len(cseq))
+    df.set_value(seq, "length", int(len_cseq))
+    df.set_value(seq, "GC", round(((cseq.count("G")+cseq.count("C")) / len_cseq) * 100.0))
+    df.set_value(seq, "AT", round(((cseq.count("A")+cseq.count("T")) / len_cseq) * 100.0))
+    for ss in kmerlist_all_seq[seq]:
+        df.set_value(seq, ss, kmerlist_all_seq[seq][ss])
+
+df.to_csv(bif+".csv",index_label='Sequence',index=True)
+
+df = pd.DataFrame(0,index=kmerlist.keys(),columns=[bif])
+for k,v in kmerlist.items():
+    df.set_value(k,bif,v)
+
+df.to_csv(bif+"_summary.csv",index_label='Kmer',index=True)
+
+# import csv
+# with open(bif+'.csv', 'wb') as csv_file:
+#     writer = csv.writer(csv_file)
+#     for key, value in kmerlist.items():
+#        writer.writerow([key, value])
+
 
