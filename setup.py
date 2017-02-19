4cf19cd07f6121e26bcbb95ed88c89bab6ab187d
Ajay Panyala <ajay.panyala@gmail.com>
2017-02-19 15:04:02 -0800

    Finalize setup.py for conda

--- setup.py
+++ setup.py
@@ -3,18 +3,9 @@ from os.path import dirname, join
 from setuptools import setup, find_packages
 
 
-def get_version(relpath):
-  """Read version info from a file without importing it"""
-  for line in io.open(join(dirname(__file__), relpath), encoding="cp437"):
-    if "__version__" in line:
-      if '"' in line:
-        return line.split('"')[1]
-      elif "'" in line:
-        return line.split("'")[1]
-
 setup(
     name='mercat',
-    version=get_version("mercat/__init__.py"),
+    version="0.1",
     author='Ajay Panyala',
     author_email='ajay@example.com',
     url='https://github.com/pnnl/mercat',
