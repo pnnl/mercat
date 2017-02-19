129dfc4a0c151e5726555a22759286f4c440b846
Ajay Panyala <ajay.panyala@gmail.com>
2017-02-19 07:58:57 -0800

    Remove bin scripts

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='mercat',
     version='0.1',
     author='Ajay Panyala',
     author_email='ajay@example.com',
-    packages=['mercat'],
+    packages=find_packages()
     #url='http://github.com/ap/mercat/',
-    scripts=['bin/mercat']
+    #scripts=['bin/mercat']
+
+
 )
